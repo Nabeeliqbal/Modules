679e8d947d4e1287dee3393f9f0127fd01627f47
nabeeliqbal <nabeeliqbal399@gmail.com>
2023-03-14 12:41:36 +0500

    My first code on github

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,52 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v107.network.Network;
+import org.openqa.selenium.devtools.v107.network.model.RequestId;
+import org.openqa.selenium.devtools.v107.network.model.Response;
+
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        WebDriver driver = new ChromeDriver();
+
+        // Setup Network to access api response
+        DevTools devTools = ((ChromeDriver) driver).getDevTools();
+        devTools.createSession();
+        devTools.send(Network.enable(Optional.empty(),Optional.empty(),Optional.empty()));
+
+        // Add listener to receive API cal response
+        devTools.addListener(Network.responseReceived(), response ->
+        {
+            Response res = response.getResponse();
+            RequestId req = response.getRequestId();
+            if (res.getStatus() == 200 && res.getUrl().equals("https://mareez-care.com/api/auth/send-verification-code")) {
+                System.out.println(res.getUrl());
+                String  responseBody = devTools.send(Network.getResponseBody(req)).getBody();
+
+                JsonObject jsonObject1 = (JsonObject) JsonParser.parseString(responseBody);
+                String accessToken = jsonObject1.get("data").getAsJsonObject().get("temp_token").getAsString();
+                driver.findElement(By.cssSelector("input[class='w-[265px] px-2 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-gray-300 focus:ring-1 focus:ring-gray-300']")).sendKeys(accessToken);
+            }
+        });
+
+        driver.get("https://mareez-care.com/doctor/login");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@placeholder='Enter phone number']")).sendKeys(" 304-5071888");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[@type='submit']")).submit();
+        Thread.sleep(3000);
+
+        // OTP
+        driver.findElement(By.xpath("//button[@type='submit']")).submit();
+        Thread.sleep(3000);
     }
 }
